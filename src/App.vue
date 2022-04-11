1f608926eeb0ce602276d0c8e7f748b132089e97
Kecar2 <kendrycarvajalsegura>
2022-04-11 09:45:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<script>
+
+import Card from '/src/Card.vue';
+
+export default {
+  components: {
+    Card
+  }
+}
+
+</script>
+
+<template>
+  <main>
+    <h1>Contactos Dr. Slump</h1>
+    <Card name="Arale" hobbies="Le gusta jugar con Gachán"
+      urlImage="https://i.pinimg.com/originals/f5/e8/bb/f5e8bb3e27fcfe21c94f3ddf2b1353cc.jpg"
+      bgColor="rgb(230,230,250)" />
+        <Card name="Gachán" hobbies="Le gusta comer coches"
+      urlImage="https://lh3.googleusercontent.com/QQBkBnr_GaHLxTdvfZTFayKQrkhIbtDXwReXx9hdBJd2p7c6g52ScYzG2TUpbsaQ4Ydp9EIPBX9uQIB6hgtaM6RSGc_choZWoPuf=s0"
+      bgColor="background-color: rgba(0, 255, 0, 0.5)" 
+      bg-color="olive"/>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/Card.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: ["urlImage", "name", "hobbies", "bgColor"]
+}
+</script>
+
+<template>
+    <div :style="{ 'backgroundColor': bgColor, 'fontWeight': 700 }" class="card">
+        <div class="card--header">
+            <img class="card--image" :src="urlImage" alt="arale">
+            <span>{{ name }}</span>
+        </div>
+        <p> {{ hobbies }} </p>
+    </div>
+</template>
+
+<style>
+.card {
+    border: 1px solid black;
+}
+
+.card--header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 1.3rem;
+    padding: 1rem;
+}
+
+.card--image {
+    width: 100px;
+}
+</style>
